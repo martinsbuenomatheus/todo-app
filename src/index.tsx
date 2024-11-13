7bdcc8135e41ddddfdbdd2e7b39494df4ffbc337
Matheus Martins Bueno <128252714+martinsbuenomatheus@users.noreply.github.com>
2024-11-13 20:09:36 -0300

    chore: change to Component Arch

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,10 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import './index.css';
 
-//import App from './ComponentArch/App';
-import App from "./CleanArch/App";
+import App from './ComponentArch/App';
+//import App from "./CleanArch/App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-//const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
   <React.StrictMode>
